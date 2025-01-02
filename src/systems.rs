58db44193d500a6f1a0a8b513144db4d88bc00c4
thatmagicalcat <thatmagicalcat@duck.com>
2025-01-02 12:28:09 +0530

    added faster collision detection

--- src/systems.rs
+++ src/systems.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use collision::find_overlapping_intervals_2d;
 
 use super::*;
@@ -34,88 +36,119 @@ pub fn handle_mouse_collision(
 #[system]
 pub fn handle_collisions(
     world: &mut SubWorld,
-    main_query: &mut Query<(&Id, &Mass, &mut Position, &mut Velocity, &ShapeInfo)>,
+    query: &mut Query<(&Id, &Mass, &mut Position, &mut Velocity, &ShapeInfo)>,
+    #[resource] qt: &QuadTree<usize>,
+    #[resource] slower_detection: &bool,
 ) {
-    let mut entities = [const { None }; 10_000];
-    main_query.for_each_mut(
-        world,
-        |(Id(id), mass, pos, vel, ShapeInfo { radius, .. })| {
-            entities[*id] = Some((mass, pos, vel, radius));
-        },
-    );
+    if !*slower_detection {
+        let mut entities = [const { None }; 20_000];
 
-    let (x_intervals, y_intervals) =
-        collision::create_intervals(entities.iter().enumerate().filter_map(|(index, info)| {
-            info.as_ref()
-                .map(|(_, pos, _, radius)| (index, &**pos, **radius))
-        }));
+        query.for_each_mut(world, |(Id(id), a, b, c, d)| {
+            entities[*id] = Some((*a, *b, *c, *d, false)); // false means no update
+        });
 
-    let overlapping = find_overlapping_intervals_2d(&x_intervals, &y_intervals);
+        for i in 0..entities.len() {
+            let Some((Mass(m1), Position(pos1), vel1, ShapeInfo { radius, .. }, _)) =
+                entities[i].as_ref()
+            else {
+                break;
+            };
 
-    for (a, b) in overlapping {
-        let ((m1, pos1, vel1, radius1), (m2, pos2, vel2, radius2)) =
-            (entities[a].as_ref().unwrap(), entities[b].as_ref().unwrap());
+            let pos1 = *pos1;
+            let m1 = *m1;
+            let vel1 = *vel1;
+            let radius = *radius;
 
-        let distance = (pos1.0 - pos2.0).length();
-        let combined_radius = **radius1 + **radius2;
+            let colliding = qt.query(pos1, radius);
 
-        let overlap = combined_radius - distance;
-        let direction = (pos1.0 - pos2.0).normalize();
-        let correction = direction * overlap / 2.0;
+            if entities[i].as_ref().unwrap().4 {
+                continue;
+            }
 
-        let (new_vel1, new_vel2) = process_collision(vel1.0, vel2.0, pos1.0, pos2.0, m1.0, m2.0);
+            for colliding_obj_id in colliding {
+                if i == colliding_obj_id || entities[colliding_obj_id].as_ref().unwrap().4 {
+                    continue;
+                }
 
-        let a = entities[a].as_mut().unwrap();
-        a.1.0 += correction;
-        a.2.0 = new_vel1;
+                let (m2, Position(pos2), vel2, shape, _) =
+                    &entities[colliding_obj_id].as_ref().unwrap();
 
-        let b = entities[b].as_mut().unwrap();
-        b.1.0 -= correction;
-        b.2.0 = new_vel2;
+                let distance = (pos1 - pos2).length();
+                let combined_radius = radius + shape.radius;
+
+                if distance > combined_radius {
+                    continue;
+                }
+
+                let (new_vel1, new_vel2) = process_collision(vel1.0, vel2.0, pos1, *pos2, m1, m2.0);
+
+                let overlap = combined_radius - distance;
+                let direction = (pos1 - pos2).normalize();
+                let correction = direction * overlap / 2.0;
+
+                let (_, pos, vel, _, update) = entities[i].as_mut().unwrap();
+                *update = true;
+                pos.0 += correction;
+                vel.0 = new_vel1;
+
+                let (_, pos, vel, _, update) = entities[colliding_obj_id].as_mut().unwrap();
+                *update = true;
+                pos.0 -= correction;
+                vel.0 = new_vel2;
+            }
+        }
+
+        query.iter_mut(world).for_each(|(id, _, pos, vel, _)| {
+            let (_, new_pos, new_vel, _, update) = entities[id.0].as_ref().unwrap();
+
+            if *update {
+                *pos = *new_pos;
+                *vel = *new_vel;
+            }
+        });
+    } else {
+        let entities = query.iter_mut(world).collect::<Vec<_>>();
+        let mut updated = [None; 20_000];
+
+        // Check collisions for all pairs
+        for i in 0..entities.len() {
+            for j in (i + 1)..entities.len() {
+                let (id1, mass1, pos1, vel1, shape1) = &entities[i];
+                let (id2, mass2, pos2, vel2, shape2) = &entities[j];
+
+                // Calculate distance and combined radius
+                let distance = (pos1.0 - pos2.0).length();
+                let combined_radius = shape1.radius + shape2.radius;
+
+                if distance < combined_radius {
+                    // Collision detected, process velocities
+                    let (new_vel1, new_vel2) =
+                        process_collision(vel1.0, vel2.0, pos1.0, pos2.0, mass1.0, mass2.0);
+
+                    let overlap = combined_radius - distance;
+                    let direction = (pos1.0 - pos2.0).normalize();
+                    let correction = direction * overlap / 2.0;
+
+                    let mut pos1 = **pos1;
+                    let mut pos2 = **pos2;
+
+                    // Move the particles apart
+                    pos1.0 += correction;
+                    pos2.0 -= correction;
+
+                    updated[id1.0] = Some((new_vel1, pos1));
+                    updated[id2.0] = Some((new_vel2, pos2));
+                }
+            }
+        }
+
+        query.iter_mut(world).for_each(|(id, _, pos, vel, _)| {
+            if let Some((new_vel, new_pos)) = updated[id.0] {
+                *vel = Velocity(new_vel);
+                *pos = new_pos;
+            }
+        });
     }
-
-    // Previous implementation
-    // let entities = query.iter_mut(world).collect::<Vec<_>>();
-    // let mut updated = [None; 10000];
-
-    // // Check collisions for all pairs
-    // for i in 0..entities.len() {
-    //     for j in (i + 1)..entities.len() {
-    //         let (id1, mass1, pos1, vel1, shape1) = &entities[i];
-    //         let (id2, mass2, pos2, vel2, shape2) = &entities[j];
-
-    //         // Calculate distance and combined radius
-    //         let distance = (pos1.0 - pos2.0).length();
-    //         let combined_radius = shape1.radius + shape2.radius;
-
-    //         if distance < combined_radius {
-    //             // Collision detected, process velocities
-    //             let (new_vel1, new_vel2) =
-    //                 process_collision(vel1.0, vel2.0, pos1.0, pos2.0, mass1.0, mass2.0);
-
-    //             let overlap = combined_radius - distance;
-    //             let direction = (pos1.0 - pos2.0).normalize();
-    //             let correction = direction * overlap / 2.0;
-
-    //             let mut pos1 = **pos1;
-    //             let mut pos2 = **pos2;
-
-    //             // Move the particles apart
-    //             pos1.0 += correction;
-    //             pos2.0 -= correction;
-
-    //             updated[id1.0] = Some((new_vel1, pos1));
-    //             updated[id2.0] = Some((new_vel2, pos2));
-    //         }
-    //     }
-    // }
-
-    // query.iter_mut(world).for_each(|(id, _, pos, vel, _)| {
-    //     if let Some((new_vel, new_pos)) = updated[id.0] {
-    //         *vel = Velocity(new_vel);
-    //         *pos = new_pos;
-    //     }
-    // });
 }
 
 #[system(for_each)]
@@ -130,17 +163,17 @@ pub fn check_wall_collision(
     ShapeInfo { radius, .. }: &ShapeInfo,
     #[resource] size: &Vector2u,
 ) {
-    if pos.0.x < 0.0 {
+    if pos.0.x - *radius < 0.0 {
         vel.0.x *= -1.0;
-        pos.0.x = 0.0;
+        pos.0.x = *radius;
     } else if pos.0.x + radius >= (size.x - WINDOW_PADDING) as f64 {
         vel.0.x *= -1.0;
         pos.0.x = (size.x - WINDOW_PADDING) as f64 - radius;
     }
 
-    if pos.0.y < 0.0 {
+    if pos.0.y - *radius < 0.0 {
         vel.0.y *= -1.0;
-        pos.0.y = 0.0;
+        pos.0.y = *radius;
     } else if pos.0.y + radius >= (size.y - WINDOW_PADDING) as f64 {
         vel.0.y *= -1.0;
         pos.0.y = (size.y - WINDOW_PADDING) as f64 - radius;
